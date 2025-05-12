f7dc618f9ea363fe75f0b2582fe8830ffc6078cb
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-05-12 09:35:48 +0600

    read and write fil eusing asynchronous

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./text/input.txt', 'utf-8');
-console.log(textIn);
+
+// Synchronous-(Blocking Code)
+// const textIn = fs.readFileSync('./text/input.txt', 'utf-8');
+// console.log(`Read File : ${textIn}`);
+
+// const writeText = `write file : ${textIn}`;
+// const textOut = fs.writeFileSync('./text/output.txt', writeText, 'utf-8');
+
+// const output = fs.readFileSync('./text/output.txt', 'utf-8');
+// console.log(`${output}`);
+
+// Asynchronous-(Non-Blocking Code)
+fs.readFile('./text/input.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log('Error : ', err);
+        return;
+    }
+    console.log(`Read File : ${data}`);
+})
+
+
+const writeText = `This is some dummy text for testing purposes.`;
+fs.writeFile('./text/output2.txt', writeText, 'utf-8', (err) => {
+    if (err) {
+        console.log('Error : ', err);
+        return;
+    }
+    console.log('File written successfully');
+})
